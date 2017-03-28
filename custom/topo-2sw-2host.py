047472ba4e46ddab8b90ff797c241ce5836642ae
Leszek Barszcz <leszek.barszcz@grupawp.pl>
2017-03-28 21:21:07 +0200

    WIP: Mesh switch topology generator. Adding hosts needed

--- custom/topo-2sw-2host.py
+++ custom/topo-2sw-2host.py
@@ -1,17 +1,11 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
+﻿﻿"""Mesh topology creator with single diagonal links
+Misses adding hosts
 """
 
 from mininet.topo import Topo
 
-class MyTopo( Topo ):
-    "Simple topology example."
+class MeshTopo( Topo ):
+    "Mesh topology example."
 
     def __init__( self ):
         "Create custom topo."
@@ -19,16 +13,50 @@ class MyTopo( Topo ):
         # Initialize topology
         Topo.__init__( self )
 
-        # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        switchesArray = []
 
-        # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        topoSize = 4
+
+        switchesArray.append(self.addSwitch('s1'))
+        switchesArray.append(self.addSwitch('s2'))
+        switchesArray.append(self.addSwitch('s3'))
+        switchesArray.append(self.addSwitch('s4'))
+
+        self.addLink(switchesArray[0], switchesArray[1])
+        self.addLink(switchesArray[3], switchesArray[1])
+        self.addLink(switchesArray[2], switchesArray[3])
+        self.addLink(switchesArray[0], switchesArray[2])
+        self.addLink(switchesArray[0], switchesArray[3])
+
+        for j in range(0, topoSize):
+            if j == 0:
+                for i in range(0, topoSize-1):
+                    switchesArray.append(self.addSwitch('s' + str(i * 2 + 5)))
+                    switchesArray.append(self.addSwitch('s' + str(i * 2 + 6)))
+                    self.addLink(switchesArray[i * 2 + 2], switchesArray[i * 2 + 4])
+                    self.addLink(switchesArray[i * 2 + 4], switchesArray[i * 2 + 5])
+                    self.addLink(switchesArray[i * 2 + 3], switchesArray[i * 2 + 5])
+                    self.addLink(switchesArray[i * 2 + 2], switchesArray[i * 2 + 5])
+            else:
+                for i in range(0, topoSize+1):
+                    switchName = 's' + str((j+1) * (topoSize + 1) + i + 1)
+                    switchesArray.append(self.addSwitch(switchName))
+
+                for i in range(0, topoSize+1):
+                    firstSwitch = 0
+                    secondSwitch = (j + 1) * (topoSize + 1) + i
+                    if j == 1:
+                        firstSwitch = j + (i * 2)
+                    else:
+                        firstSwitch = j * (topoSize + 1) + i
+
+                    #vertical links
+                    self.addLink(switchesArray[firstSwitch], switchesArray[secondSwitch])
+                    if i < topoSize:
+                        #diagonal links
+                        self.addLink(switchesArray[firstSwitch], switchesArray[secondSwitch+1])
+                        #horizontal links
+                        self.addLink(switchesArray[secondSwitch], switchesArray[secondSwitch+1])
 
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+topos = { 'meshtopo': ( lambda: MeshTopo() ) }
